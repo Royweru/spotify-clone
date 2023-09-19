a9ac465b3e9b84c07643f0e4d88d5941e595bc0e
Royweru <royezi17@gmail.com>
2023-09-19 22:11:48 +0300

    modified:   app/(site)/page.tsx 	new file:   app/components/Box.tsx 	new file:   app/components/Library.tsx 	new file:   app/components/Sidebar.tsx 	new file:   app/components/SidebarItem.tsx 	modified:   app/layout.tsx 	modified:   package-lock.json 	modified:   package.json

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -2,10 +2,10 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <div className=' text-3xl font-bold '>
-      Hello site
+   
+     <div className=' text-3xl font-bold text-green-200'>
+      Hello spotify again!
      </div>
-    </main>
+   
   )
 }
