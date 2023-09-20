9eeaab793f6f40ad8592f4013e1323b56f0d0b77
Royweru <royezi17@gmail.com>
2023-09-20 07:29:22 +0300

    modified:   app/(site)/page.tsx 	new file:   app/components/Button.tsx 	new file:   app/components/Header.tsx 	new file:   app/components/ListItem.tsx 	new file:   public/images/liked.jpeg

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,10 +1,47 @@
 import Image from 'next/image'
-
+import Header from '../components/Header'
+import ListItem from '../components/ListItem'
 export default function Home() {
   return (
    
-     <div className=' text-3xl font-bold text-green-200'>
-      Hello spotify again!
+     <div className=' 
+      bg-neutral-900
+      rounded-lg
+      h-full
+      w-full
+      overflow-hidden
+      overflow-y-auto
+     '>
+      <Header>
+        <div className=' mb-2'>
+          <h1
+           className='
+            text-white
+            text-3xl
+            font-semibold
+           '
+          >
+            Welcome back
+          </h1>
+          <div
+           className='
+            grid
+            grid-cols-1
+            sm:grid-cols-2
+            xl:grid-cols-3
+            2xl:grid-cols-4
+            gap-3
+            mt-4
+           '
+          >
+           <ListItem 
+            image='/images/liked.jpeg'
+            name='Liked songs'
+            href=''
+           />
+          </div>
+        </div>
+      </Header>
      </div>
    
   )
